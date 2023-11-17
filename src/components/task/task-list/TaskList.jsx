2d47a0d412cad7c0c30aec34711c1ec1f581b9b6
Xavier Lopez <xlopezmin@gmail.com>
2023-11-17 17:12:26 +0100

    feat(tasklist): wip delete

--- src/components/task/task-list/TaskList.jsx
+++ src/components/task/task-list/TaskList.jsx
@@ -7,6 +7,7 @@ import imageNotification from "../../../assets/img/campana.svg";
 import imageNote from "../../../assets/img/nota-adhesiva.svg";
 import imageCategory from "../../../assets/img/grabacion.svg";
 import imageFavorite from "../../../assets/img/estrella.svg";
+import imageDelete from "../../../assets/img/basura.svg";
 
 
 function TaskList () {
@@ -36,12 +37,42 @@ function TaskList () {
         }
     }
 
+    const handlerDeleteTaskOnClick = async(id) => {
+        id = 0;
+        try {
+            console.log("deleteTask():", id);
+            const url = "http://localhost:3000/todo/" + id;
+
+            const response = await fetch(url, {
+                method: "DELETE",
+                headers: {"Content-Type": "application/json"}
+            });
+
+            if (response.ok) {
+                console.log("deleteTask().response.ok:", response.ok);
+            } else {
+                console.log("deleteTask().response.ok:", response.ok);
+            }
+        } catch(error) {
+            console.log("deleteTask().id:", id);
+        }
+    }
+
     return (
         <>
-
-
-
-
+            <div>
+                <span>Título tarea 1</span>
+                <button key="0" onClick={handlerDeleteTaskOnClick}>
+                    <img key="0" className={styles.images} src={imageDelete} alt="Eliminar tarea"/>
+                </button>
+            </div>
+            <div>
+                <span>Título tarea 2</span>
+                <button key="1" onClick={handlerDeleteTaskOnClick}>
+                    <img key="1" className={styles.images} src={imageDelete} alt="Eliminar tarea"/>
+                </button>
+            </div>
+            
         </>
     )
 }
